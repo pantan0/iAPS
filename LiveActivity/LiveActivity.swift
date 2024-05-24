e2dd7d7a4b557df39f886e2861496c1b0d4b138c
Joseph Pantano <j.pantano@outlook.com>
2024-05-23 21:03:52 -0400

    Update LiveActivity.swift

--- LiveActivity/LiveActivity.swift
+++ LiveActivity/LiveActivity.swift
@@ -157,7 +157,7 @@ struct LiveActivity: Widget {
                 }
             }
             .widgetURL(URL(string: "freeaps-x://"))
-            .keylineTint(Color.purple)
+            .keylineTint(Color.mint)
             .contentMargins(.horizontal, 0, for: .minimal)
             .contentMargins(.trailing, 0, for: .compactLeading)
             .contentMargins(.leading, 0, for: .compactTrailing)
